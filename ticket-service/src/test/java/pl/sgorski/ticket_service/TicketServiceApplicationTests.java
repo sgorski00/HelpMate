2c4dbcd01a80dbbed1aef9cb5ad2dfb070dbf8f7
Sebastian Górski <sebastiangorski00@gmail.com>
2025-09-11 14:17:05 +0200

    test: disabled defualt app tests

--- ticket-service/src/test/java/pl/sgorski/ticket_service/TicketServiceApplicationTests.java
+++ ticket-service/src/test/java/pl/sgorski/ticket_service/TicketServiceApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
+@Disabled("Disabled until proper test database is set up")
 class TicketServiceApplicationTests {
 
 	@Test
